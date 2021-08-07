cefeb03e4121738aec7045940ea18eece8854724
Fernando-Linhares <fernandomadethat@gmail.com>
2021-08-07 16:08:58 -0300

    final commit

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Product;
+
 /**
 ** @Route("/products", name="products")
 **/
 class ProductsController extends AbstractController
 {
     /**
-     * @Route("/", name="index", METHOD={"GET"})
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index()
     {
@@ -20,28 +21,54 @@ class ProductsController extends AbstractController
         return $this->json(['data' => $products]);
     }
 
-
     /**
      * @Route("/{id}", name="update", methods={"GET"})
      */
     public function show($id)
     {
-
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        return $this->json($product);
     }
 
     /**
-     * @Route("/{id}/update", name="update", methods={"PUT"})
+     * @Route("/{id}/update", name="update", methods={"PUT", "PATCH"})
      */
-    public function update($id)
+    public function update($id, Request $request)
     {
+        $data = $request->request->all();
 
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($id);
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $product->setCategory($data['category']);
+        $product->setCreatedAt(new \DateTimeImmutable('now', new \DateTimezone('America/Sao_Paulo')));
+        $product->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimezone('America/Sao_Paulo')));
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($product);
+        $doctrine->flush();
+
+        return $this->json(['data'=>'product updated succesfully']);
     }
 
     /**
      * @Route("/create", name="create", methods={"POST"})
      */
-    public function create()
+    public function create(Request $request)
     {
+        $data = $request->request->all();
+        $product = new Product;
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $product->setCategory($data['category']);
+        $product->setCreatedAt(new \DateTimeImmutable('now', new \DateTimezone('America/Sao_Paulo')));
+        $product->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimezone('America/Sao_Paulo')));
+
+        $doctrine = $this->getDoctrine()->getManager();
+        $doctrine->persist($product);
+        $doctrine->flush();
+
+        return $this->json(['data'=>'product created succesfully']);
         
     }
 
@@ -50,14 +77,12 @@ class ProductsController extends AbstractController
      */
     public function delete($id)
     {
-        
-    }
-
-    /**
-     * @Route("/{id}/{col}/patch", name="patch", methods="{PATCH}")
-     */
-    public function patch($id, $col)
-    {
-        
+        $doctrine = $this->getDoctrine();
+    
+        $product = $doctrine->getRepository(Product::class)->find($id);
+        $manager = $doctrine->getManager();
+        $manager->remove($product);
+        $manager->flush();
+        return $this->json(['message'=>'item deleted succesfully']);
     }
 }
